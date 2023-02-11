93456cf4d0f8e1ac999799b775d36acefefe881d
Bek <nematjanov95@gmail.com>
2023-02-11 14:14:44 +0500

    added bottom navigation bar

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 class HomeController extends ChangeNotifier {
+   int currentIndex = 0;
+
+  setIndex(int index) {
+    currentIndex = index;
+    notifyListeners();
+  }
 
 }
--- lib/view/pages/general_page.dart
+++ lib/view/pages/general_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram/view/pages/home_page.dart';
+import 'package:provider/provider.dart';
+
+import '../../controller/home_controller.dart';
 
 class GeneralPage extends StatefulWidget {
   const GeneralPage({super.key});
@@ -7,9 +13,43 @@ class GeneralPage extends StatefulWidget {
   State<GeneralPage> createState() => _GeneralPageState();
 }
 
+
+
 class _GeneralPageState extends State<GeneralPage> {
+  List<Widget> mainPages = [
+    const HomePage(),
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+  ];
+
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: IndexedStack(
+        index: context.watch<HomeController>().currentIndex,
+        children: mainPages,
+      ),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.transparent,
+     bottomNavigationBar: BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: [
+      const BottomNavigationBarItem(icon: Icon(Icons.home)),
+      const BottomNavigationBarItem(icon: Icon(Icons.search)),
+      const BottomNavigationBarItem(icon: Icon(Icons.add_box_outlined)),
+      BottomNavigationBarItem(icon: SvgPicture.asset('assets/svg/Icons.svg')),
+      BottomNavigationBarItem(icon: Container(
+        height: 25.h,
+        width: 25.w,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(image: AssetImage('assets/images/me.jpeg'),fit: BoxFit.cover)
+        ),
+      )),
+     ]),
+    );
   }
 }
