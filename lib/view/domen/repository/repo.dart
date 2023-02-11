70f69d2ada57019423b302fe303b903623417aed
Bek <nematjanov95@gmail.com>
2023-02-11 21:00:14 +0500

    fixing

--- lib/view/domen/repository/repo.dart
+++ lib/view/domen/repository/repo.dart
@@ -28,4 +28,32 @@ abstract class GetInfo {
       print(e);
     }
   }
+
+
+  static search(String text) async {
+    Dio _dio = Dio(BaseOptions(
+      headers: {
+        'X-RapidAPI-Key': ' 8d73d12368msh94d59547af4d732p151035jsn05efea604202',
+       
+        //91d31d4cb8mshdd2eaed2c3bf3b5p1bc6d3jsn9e4eb668d330
+        'X-RapidAPI-Host': 'instagram47.p.rapidapi.com'
+      },
+      baseUrl: "https://instagram47.p.rapidapi.com",
+    ))
+      ..interceptors.add(LogInterceptor(
+          responseBody: true,
+          requestBody: true,
+          requestHeader: true,
+          responseHeader: false));
+    try {
+      var response = await _dio.get('/search?search=$text');
+      print(response.data.toString());
+
+      return singleuserFromJson(response.data);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
 }
--- lib/view/pages/search_page.dart
+++ lib/view/pages/search_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:instagram/view/style/style.dart';
+
+import '../domen/model/search_model.dart';
+import '../domen/repository/repo.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -9,6 +13,26 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  late TextEditingController searchController;
+  Search? search;
+  @override
+  void initState() {
+    getInfo();
+    searchController =TextEditingController();
+    super.initState();
+  }
+
+  getInfo() async {
+    search = await GetInfo.search(searchController.text);
+    print('User: $search');
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +42,7 @@ class _SearchPageState extends State<SearchPage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
               child: TextFormField(
+                controller: searchController,
                 decoration: InputDecoration(
                     prefixIcon: Icon(Icons.search),
                     fillColor: Color.fromARGB(255, 200, 197, 197),
@@ -33,7 +58,11 @@ class _SearchPageState extends State<SearchPage> {
                         ),
                         borderRadius: BorderRadius.circular(20))),
               ),
-            )
+            ),
+            ListView.builder(itemBuilder: (context,index)=>Container(
+                height: 52.h,
+                width: 374.w,
+            ))
           ],
         ),
       ),
